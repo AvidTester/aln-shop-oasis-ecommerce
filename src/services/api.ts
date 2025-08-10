63e0848abb7ec0050aad92901ef300dc21bd587d
kxh03 <klintixh23@gmail.com>
2025-08-10 13:49:34 +0200

    Update api.ts

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,5 +1,5 @@
 
-const API_BASE_URL = 'http://localhost:5000/api';
+const API_BASE_URL = 'https://aln-shop.onrender.com/api'; // http://localhost:5000/api
 
 export const apiRequest = async (endpoint: string, options: RequestInit = {}) => {
   const url = `${API_BASE_URL}${endpoint}`;
